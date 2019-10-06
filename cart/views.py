805d2b3840c49436d1564f9d6e322eb44bfa0019
muj-programmer <nick.muj.programmer@gmail.com>
2019-10-06 22:18:38 +0530

    completed cart app configurations

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from cart.cart import Cart
+from cart.forms import CartAddProductForm
+from shop.recommender import Recommender
 
-# Create your views here.
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product,
+                 quantity=cd['quantity'],
+                 update_quantity=cd['update'])
+    return redirect('cart:cart_detail')
+
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(
+            initial={
+                'quantity': item['quantity'],
+                'update': True
+            })
+    # will see about coupons
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    recommended_products = r.suggest_products_for(cart_products, max_results=4)
+    return render(request, 'cart/detail.html', {
+        'cart': cart,
+        'recommended_products': recommended_products
+    })
