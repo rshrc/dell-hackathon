e4a4014f2ab3f204d3bf245248c495343168e896
muj-programmer <nick.muj.programmer@gmail.com>
2019-10-06 23:23:21 +0530

    added script to setup recommender engine

---
+++ setup.py
@@ -0,0 +1,15 @@
+# Script to setup a demo recommending system
+
+from shop.models import Product
+from shop.recommender import Recommender
+
+r = Recommender()
+
+dell_inspiron = Product.objects.get(name='Dell Inspiron')
+dell_laptop = Product.objects.get(name='Dell Laptop')
+dell_monitor = Product.objects.get(name='Dell Monitor')
+dell_keyboard = Product.objects.get(name='Dell Keyboard')
+
+r.products_bought([dell_monitor, dell_keyboard])
+r.products_bought([dell_inspiron, dell_laptop])
+r.products_bought([dell_inspiron, dell_keyboard, dell_monitor])
