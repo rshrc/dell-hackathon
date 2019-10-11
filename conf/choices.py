2e5fd7bdc53390c2554813ab789414a99439c41b
muj-programmer <nick.muj.programmer@gmail.com>
2019-10-11 18:22:05 +0530

    added conf dir

---
+++ conf/choices.py
@@ -0,0 +1,14 @@
+PROFILE_CHOICES = (
+    ('Developer', 'Developer'),
+    ('Artist', 'Artist'),
+    ('Gamer', 'Gamer'),
+    ('Movie Buff', 'Movie Buff'),
+    ('Student', 'Student'),
+    ('Normal User', 'Normal User'),
+)
+
+GENDER_CHOICES = (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+    ('Other', 'Other'),
+)
---
+++ conf/fields.py
@@ -0,0 +1,25 @@
+PRODUCT_FIELDS = (
+    'category',
+    'name',
+    'slug',
+    'image',
+    'description',
+    'price',
+    'available',
+    'created',
+    'updated',
+    'conversion_rate',
+)
+
+PRODUCT_FIELDS_API = (
+    'category__name',
+    'name',
+)
+
+USER_PROFILE_FIELDS = (
+    'birthday',
+    'city',
+    'country',
+    'gender',
+    'occupation',
+)
