472a1329dd89e1a41f53d8a0198b68164c6ce08c
Rishi Banerjee <rishieric91@gmail.com>
2019-10-12 18:27:44 +0530

    order url fix

--- orders/views.py
+++ orders/views.py
@@ -20,11 +20,11 @@ def order_create(request):
             cart.clear()
             # launch asynchronous task
             order_created.delay(order.id)
-            return render(request, 'orders/order/created.html',
+            return render(request, 'order/created.html',
                           {'order': order})
     else:
         form = OrderCreateForm()
-    return render(request, 'orders/order/create.html', {
+    return render(request, 'order/create.html', {
         'cart': cart,
         'form': form
     })
