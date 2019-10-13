924209cf7a168745196fb0371d2ed02ab196215f
muj-programmer <nick.muj.programmer@gmail.com>
2019-10-14 02:09:51 +0530

    populate db with products

--- populate.py
+++ populate.py
@@ -1,14 +1,14 @@
+from shop.models import Category, Product
+from users.models import User
 import os
 import django
 from django.template.defaultfilters import slugify
-from conf.choices import CATEGORY_CHOICES
+from conf.choices import CATEGORY_CHOICES, PRODUCT_CHOICES
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 django.setup()
 
 # Models
-from users.models import User
-from shop.models import Category
 
 # create categories
 for category in CATEGORY_CHOICES:
@@ -16,3 +16,8 @@ for category in CATEGORY_CHOICES:
 
 # create admin user
 User.objects.create_superuser('admin@work.com', 'password')
+
+# create products
+for product in PRODUCT_CHOICES:
+    Product(product['category'], product['name'], slugify(
+        product['name']), product['small_image'], product['large_image'], product['description'], product['price'], product['available'], product['small_image']).save()
