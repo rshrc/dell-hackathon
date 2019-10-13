6d843011b063c864e3e86f195a99dd301e19c466
muj-programmer <nick.muj.programmer@gmail.com>
2019-10-14 03:24:24 +0530

    completed implementation of populate script

--- conf/choices.py
+++ conf/choices.py
@@ -7,4 +7,4 @@ CATEGORY_CHOICES = ['Laptop', 'Gaming', 'Workstation',
                     'Monitor', 'Keyboard', 'Mouse']
 
 PRODUCT_CHOICES = [{'category': 'Laptop', 'name': 'Dell Inspiron 7570',
-                    'small_image': '/conf/img/dell-inspiron-7570-small.jpeg', 'large_image': '/conf/img/dell-inspiron-7570-large.jpeg', 'description': '- AMD Ryzen™ 5 2500U Mobile Processor with Radeon™ Vega 8 Graphics\n- Windows 10 Home 64-bit English\n- Integrated graphics with AMD APU\n- 17.3-inch HD+ (1600 x 900) Anti-Glare LED-Backlit Non-touch Display\n- 8GB, 2x4GB, DDR4, 2666MHz\n- 1TB 5400 rpm 2.5" SATA Hard Drive', 'price': 35000, 'available': True}]
+                    'small_image': 'dell-inspiron-7570-small.jpeg', 'large_image': 'dell-inspiron-7570-large.jpeg', 'description': '- AMD Ryzen™ 5 2500U Mobile Processor with Radeon™ Vega 8 Graphics\n- Windows 10 Home 64-bit English\n- Integrated graphics with AMD APU\n- 17.3-inch HD+ (1600 x 900) Anti-Glare LED-Backlit Non-touch Display\n- 8GB, 2x4GB, DDR4, 2666MHz\n- 1TB 5400 rpm 2.5" SATA Hard Drive', 'price': 35000, 'available': True}]
--- populate.py
+++ populate.py
@@ -1,14 +1,20 @@
-from shop.models import Category, Product
-from users.models import User
 import os
 import django
 from django.template.defaultfilters import slugify
 from conf.choices import CATEGORY_CHOICES, PRODUCT_CHOICES
+from datetime import date
+import shutil
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+IMG_DIR = os.path.join(BASE_DIR, 'conf', 'img')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 django.setup()
 
 # Models
+from shop.models import Category, Product
+from users.models import User
 
 # create categories
 for category in CATEGORY_CHOICES:
@@ -17,7 +23,30 @@ for category in CATEGORY_CHOICES:
 # create admin user
 User.objects.create_superuser('admin@work.com', 'password')
 
+today = date.today()
+# create media dir
+os.mkdir(MEDIA_DIR)
+# create products
+os.mkdir(os.path.join(MEDIA_DIR, 'products'))
+# create small images dir
+os.mkdir(os.path.join(MEDIA_DIR, 'products', 'small'))
+os.mkdir(os.path.join(MEDIA_DIR, 'products', 'small', str(today.year)))
+os.mkdir(os.path.join(MEDIA_DIR, 'products', 'small', str(today.year), str(today.month)))
+os.mkdir(os.path.join(MEDIA_DIR, 'products', 'small', str(today.year), str(today.month), str(today.day)))
+
+# create large images dir
+os.mkdir(os.path.join(MEDIA_DIR, 'products', 'large'))
+os.mkdir(os.path.join(MEDIA_DIR, 'products', 'large', str(today.year)))
+os.mkdir(os.path.join(MEDIA_DIR, 'products', 'large', str(today.year), str(today.month)))
+os.mkdir(os.path.join(MEDIA_DIR, 'products', 'large', str(today.year), str(today.month), str(today.day)))
+
 # create products
 for product in PRODUCT_CHOICES:
-    Product(product['category'], product['name'], slugify(
-        product['name']), product['small_image'], product['large_image'], product['description'], product['price'], product['available'], product['small_image']).save()
+    # for small image
+    path = shutil.copy(os.path.join(IMG_DIR, product['small_image']), os.path.join(MEDIA_DIR, 'products', 'small', str(today.year), str(today.month), str(today.day)))
+    small_image_path = f'{os.sep}'.join(path.split(os.sep)[7:])
+    # for large image
+    path = shutil.copy(os.path.join(IMG_DIR, product['large_image']), os.path.join(MEDIA_DIR, 'products', 'large', str(today.year), str(today.month), str(today.day)))
+    large_image_path = f'{os.sep}'.join(path.split(os.sep)[7:])
+    Product(category=Category.objects.get(name=product['category']), name=product['name'], slug=slugify(
+        product['name']), small_image=small_image_path, large_image=large_image_path, description=product['description'], price=product['price'],).save()
