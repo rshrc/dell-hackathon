9b57c4856e71a64d531eabcbf6fcda5fa9f0946d
muj-programmer <nick.muj.programmer@gmail.com>
2019-10-14 01:46:39 +0530

    hotfix

--- users/models.py
+++ users/models.py
@@ -37,7 +37,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
