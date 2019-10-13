1db2c35c47d3d4ca370d936b413cd0a6d9286cc5
muj-programmer <nick.muj.programmer@gmail.com>
2019-10-13 19:44:30 +0530

    modified cart template

--- cart/urls.py
+++ cart/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from cart.views import cart_add, cart_detail, cart_remove
+from . import views
 
 app_name = 'cart'
 
 urlpatterns = [
-    path('', cart_detail, name='cart_detail'),
-    path('add/<int:product_id>/', cart_add, name='cart_add'),
-    path('remove/<int:product_id>/', cart_remove, name='cart_remove'),
+    path('', views.cart_detail_page, name='cart_detail_page'),
+    path('add/<int:product_id>/', views.cart_add, name='cart_add'),
+    path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Product
+from shop.views import get_categories
 from cart.cart import Cart
 from cart.forms import CartAddProductForm
 from shop.recommender import Recommender
@@ -16,29 +17,32 @@ def cart_add(request, product_id):
         cart.add(product=product,
                  quantity=cd['quantity'],
                  update_quantity=cd['update'])
-    return redirect('cart:cart_detail')
+    return redirect('cart:cart_detail_page')
 
 
 def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
-    return redirect('cart:cart_detail')
+    return redirect('cart:cart_detail_page')
 
 
-def cart_detail(request):
+def cart_detail_page(request):
     cart = Cart(request)
+
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(
             initial={
                 'quantity': item['quantity'],
                 'update': True
             })
-    # will see about coupons
+
     r = Recommender()
     cart_products = [item['product'] for item in cart]
     recommended_products = r.suggest_products_for(cart_products, max_results=4)
-    return render(request, 'cart/detail.html', {
+
+    return render(request, 'cart/cart_detail_page.html', {
         'cart': cart,
-        'recommended_products': recommended_products
+        'recommended_products': recommended_products,
+        'categories': get_categories()
     })
